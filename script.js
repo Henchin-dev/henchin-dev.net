018cde08a15ad0dc8c142698017c66b4d54db5f4
Henchin-dev <harjiimesh2@gmail.com>
2024-12-01 02:14:40 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const Smash_info = document.getElementById("Smash_info")
+const Smash_text = document.getElementById("Smash_text")
+const price_title2 = document.getElementById("price_title2")
+
+function change(){
+  document.getElementById("Smash_info").innerText = "When you need to release it all, step into our soundproof Screaming Room—a safe, controlled space to let out your frustrations. Grab markers and paints to write or draw your unspoken thoughts on our freedom walls and express yourself fully."
+  document.getElementById("Smash_text").innerText = "Screaming Room"
+  document.getElementById("price_title2").innerText = ""
+  document.getElementById("price_title23").innerText = ""
+  document.getElementById("price_title1").innerText = "Screaming Room (P499)"
+  document.getElementById("price_title12").innerText = "• 20 mins to scream it out. \n • 1 person only \n • Use of paint, brushes, and markers on freedom wall."
+}
+
+function change2(){
+  document.getElementById("Smash_info").innerText = "Enter our Smash Room, where the only thing on the \n menu is glass cups—ready for you to break. Feel the \n satisfaction of each smash as you let go of the week’s \nfrustrations."
+  document.getElementById("Smash_text").innerText = "Smash Room"
+  document.getElementById("price_title2").innerText = "Smash Room Group (1599)"
+  document.getElementById("price_title23").innerText = "• 20 mins to smash \n • 3-5 Person • \n 80 glass cups"
+  document.getElementById("price_title1").innerText = "Smash Solo Room (P399)"
+  document.getElementById("price_title12").innerText = "• 20 Mins to Smash \n • 1 person only \n • 15 glass cups"
+}
